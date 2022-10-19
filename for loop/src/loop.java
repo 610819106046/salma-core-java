54eef50fb5b4a6431ddd05620db95fb91e1e5c62
ELCOT <ELCOT@Lenovo>
2022-10-19 11:54:17 +0530

    third commit

---
+++ for loop/src/loop.java
@@ -0,0 +1,13 @@
+
+public class loop {
+
+	public static void main(String[] args) {
+for(int j=1;j<=1000;j++) //(int j=1000;j<=1;j--)
+{
+	System.out.print("value of j:");
+	System.out.println();
+}
+	}
+	
+
+}

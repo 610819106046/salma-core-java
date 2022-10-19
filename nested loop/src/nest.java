2b4c48b24c91f621a3c1376a3c799dcc0b6ed88e
ELCOT <ELCOT@Lenovo>
2022-10-19 11:31:43 +0530

    second commit

---
+++ nested loop/src/nest.java
@@ -0,0 +1,13 @@
+
+public class nest {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 100; i++) {
+			System.out.print("Factors of " + ": ");
+			for(int j = 2;j < i;j++)
+			if((i%j) == 0) System.out.print(j + " ");
+				System.out.println();
+	}
+}
+}
+
